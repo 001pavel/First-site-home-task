fcab4cb879655da1784d6e0d12e38e459817bbec
PavelKrak <tropicpost@mail.ru>
2022-11-19 15:42:11 +0300

    добавил файл js

---
+++ script.js
@@ -0,0 +1,7 @@
+let yourName = prompt("Приветствую тебя на этой страничке любитель гречки и скажите как вас зовут?", "Сюда писать ваше имя");
+
+function printYourName(name) {
+    alert(`Будь здоров, ${name}`);
+}
+
+printYourName(yourName);
